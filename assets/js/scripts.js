a09374d936266d42a92699d4f63be8441ff23b9f
Irkham <irkham.10122222@mahasiswa.unikom.ac.id>
2023-01-22 15:44:31 +0700

    Move some file to spesific folder

---
+++ assets/js/scripts.js
@@ -0,0 +1,26 @@
+function openWarna(namawarna) {
+    var i;
+    var x = document.getElementsByClassName("warna");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(namawarna).style.display = "block";
+}
+
+let docTitle = document.title;
+window.addEventListener("blur", function () {
+    document.title = "Come back :(";
+});
+window.addEventListener("focus", function () {
+    document.title = docTitle;
+});
+
+if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+    )
+) {
+    alert("This website is only for desktop. Please use desktop.");
+    window.location.href =
+        "https://10122222.github.io/favcolors/mobile.html";
+}
